30b5e1bbdec57454ebda6bbe8c54e4c242899048
Raul de Heer <raul@supertask.nl>
2020-03-05 14:46:38 +0100

    Add teachers entity

---
+++ database/migrations/2020_03_05_132947_hip_7_add_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Hip7AddTeachersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 30);
+            $table->string('last_name', 60);
+            $table->string('email')->unique();
+            $table->string('abbreviation')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('subject_teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('subject_id')->unsigned();
+            $table->integer('teacher_id')->unsigned();
+            $table->foreign('subject_id')->references('id')->on('subjects');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('subject_teachers');
+    }
+}
