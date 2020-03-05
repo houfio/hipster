65abbe6f8c4d71358f55efc44914f54b4fb742f4
houfio <26674115+houfio@users.noreply.github.com>
2020-03-05 11:11:18 +0100

    Fix migrations

--- database/migrations/2020_03_05_093241_hip-1-add-entities.php
+++ database/migrations/2020_03_05_093241_hip-1-add-entities.php
@@ -8,74 +8,68 @@ class Hip1AddEntities extends Migration
 {
     public function up()
     {
-        Schema::create('semesters', function (Blueprint $table) {
-            $table->integer('semester')->unsigned()->primary();
-        });
-
         Schema::create('periods', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
-            $table->integer('semester')->unsigned();
+            $table->increments('id');
+            $table->integer('period');
+            $table->integer('semester');
         });
 
         Schema::create('groups', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
+            $table->increments('id');
+            $table->integer('period_id')->unsigned();
             $table->string('name', 45);
-            $table->integer('period')->unsigned();
-            $table->foreign('period')->references('id')->on('periods');
-        });
-
-        Schema::create('roles', function (Blueprint $table) {
-            $table->string('role', 12)->primary();
+            $table->foreign('period_id')->references('id')->on('periods');
         });
 
         Schema::create('exams', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
+            $table->increments('id');
             $table->string('name', 80);
             $table->string('description');
             $table->dateTime('start_date');
             $table->dateTime('end_date');
+            $table->timestamps();
         });
 
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
+            $table->increments('id');
+            $table->integer('group_id')->unsigned();
+            $table->integer('role');
             $table->string('first_name', 30);
             $table->string('last_name', 60);
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role', 12);
-            $table->integer('group_id')->unsigned();
-            $table->foreign('role')->references('role')->on('roles');
             $table->foreign('group_id')->references('id')->on('groups');
+            $table->timestamps();
         });
 
         Schema::create('subjects', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
+            $table->increments('id');
+            $table->integer('period_id')->unsigned();
+            $table->integer('exam_id')->unsigned();
             $table->string('name', 45);
             $table->string('description');
-            $table->integer('credits')->unsigned();
-            $table->integer('period')->unsigned();
-            $table->integer('exam_id')->unsigned();
-            $table->foreign('period')->references('id')->on('periods');
+            $table->integer('credits');
+            $table->foreign('period_id')->references('id')->on('periods');
             $table->foreign('exam_id')->references('id')->on('exams');
+            $table->timestamps();
         });
 
         Schema::create('user_exams', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('exam_id')->unsigned();
             $table->decimal('grade', 3, 1)->nullable();
             $table->string('file')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('exam_id')->references('id')->on('exams');
+            $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('semesters');
         Schema::dropIfExists('periods');
         Schema::dropIfExists('groups');
-        Schema::dropIfExists('roles');
         Schema::dropIfExists('exams');
         Schema::dropIfExists('users');
         Schema::dropIfExists('subjects');
