bce148b63d968922c72863770e6249cb119ad32d
Raul de Heer <raul@supertask.nl>
2020-03-20 16:39:24 +0100

    Add parameter types

--- app/Traits/Encryptable.php
+++ app/Traits/Encryptable.php
@@ -8,12 +8,12 @@ trait Encryptable
 {
     public array $encryptedValues = [];
 
-    private function addToEncryptedValues($key, $value): void
+    private function addToEncryptedValues(string $key, string $value): void
     {
         $this->encryptedValues[$key] = $value;
     }
 
-    public function getAttribute($key)
+    public function getAttribute(string $key)
     {
         $value = parent::getAttribute($key);
 
@@ -25,7 +25,7 @@ trait Encryptable
         return $value;
     }
 
-    public function setAttribute($key, $value)
+    public function setAttribute(string $key, string $value)
     {
         if (in_array($key, $this->encryptable)) {
             $this->addToEncryptedValues($key, $value);
