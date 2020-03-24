da4e16c74c2c6aab5cd0039abdd27504317e545b
houfio <26674115+houfio@users.noreply.github.com>
2020-03-24 23:20:17 +0100

    Lint

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,22 +11,11 @@
     @csrf
     <div class="form-group">
       <label for="email">Email address</label>
-      <input
-        id="email"
-        type="email"
-        class="form-control"
-        name="email"
-        value="{{ old('email') }}"
-      />
+      <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"/>
     </div>
     <div class="form-group">
       <label for="password">Password</label>
-      <input
-        id="password"
-        type="password"
-        class="form-control"
-        name="password"
-      />
+      <input id="password" type="password" class="form-control" name="password"/>
     </div>
     <div class="custom-control custom-checkbox">
       <input type="checkbox" class="custom-control-input" id="remember" {{ old('remember') ? 'checked' : '' }}/>
