3e15ad0d4bf21b8fb79c9e763f69381967b689d6
houfio <26674115+houfio@users.noreply.github.com>
2020-03-24 15:29:00 +0100

    Fix duplicate url

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/exams/{subject}', 'HomeController@exams');
+Route::get('/~/{subject}', 'HomeController@exams');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
