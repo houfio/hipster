2e5b860f57be42ad20b95203aa641c66677c4ec4
houfio <26674115+houfio@users.noreply.github.com>
2020-03-24 23:11:57 +0100

    Prettify subject create page

--- resources/views/subject/create.blade.php
+++ resources/views/subject/create.blade.php
@@ -1,46 +1,30 @@
 @extends('layouts.app')
 
+@section('title', 'Create subject')
+
+@section('actions')
+  <a href="{{ action('SubjectController@index') }}" class="btn btn-light">Cancel</a>
+  <x-form-button id="create-form" type="primary">Create</x-form-button>
+@endsection
+
 @section('content')
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        @if (session('status'))
-          <div class="alert alert-success" role="alert">
-            {{ session('status') }}
-          </div>
-        @endif
-        @if($errors)
-          @foreach ($errors->all() as $error)
-            <div class="alert alert-danger" role="alert">{{ $error }}</div>
-          @endforeach
-        @endif
-        <div class="card">
-          <div class="card-header">Create subject</div>
-          <div class="card-body">
-            <form method="post" action="{{ action('SubjectController@store') }}">
-              @csrf
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="name">Name</label>
-                  <input value="{{ old('name') }}" type="text" class="form-control" name="name" id="name">
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="credits">Credits</label>
-                  <input value="{{ old('credits') }}" type="text" class="form-control" name="credits" id="credits">
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-12">
-                  <label for="description">Description</label>
-                  <textarea type="text" class="form-control" name="description" id="description">{{ old('description') }}</textarea>
-                </div>
-              </div>
-              <input type="submit" class="btn btn-secondary" value="Create">
-              <a href="{{ action('SubjectController@index') }}" class="btn btn-secondary">Cancel</a>
-            </form>
-          </div>
-        </div>
+  <form method="post" id="create-form" action="{{ action('SubjectController@store') }}">
+    @csrf
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="name">Name</label>
+        <input value="{{ old('name') }}" type="text" class="form-control" name="name" id="name"/>
+      </div>
+      <div class="form-group col-md-6">
+        <label for="credits">Credits</label>
+        <input value="{{ old('credits') }}" type="number" class="form-control" name="credits" id="credits"/>
       </div>
     </div>
-  </div>
+    <div class="form-row">
+      <div class="form-group col-md-12 mb-0">
+        <label for="description">Description</label>
+        <textarea type="text" class="form-control" name="description" id="description">{{ old('description') }}</textarea>
+      </div>
+    </div>
+  </form>
 @endsection
