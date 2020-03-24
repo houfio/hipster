6c5d97b6c07e632b6bad7f9fb22738ad2f9172a9
Raul de Heer <raul@supertask.nl>
2020-03-24 18:42:54 +0100

    Add ordering of deadlines

--- app/Http/Controllers/DeadlineController.php
+++ app/Http/Controllers/DeadlineController.php
@@ -11,7 +11,7 @@ class DeadlineController extends Controller
     {
         $page = (int)$request->query('page');
         $pages = Exam::where('due_on', '!=', null)->count() / 10;
-        $exams = Exam::where('due_on', '!=', null)->offset($page * 10)->limit(10)->get();
+        $exams = Exam::where('due_on', '!=', null)->orderBy('finished', 'asc')->offset($page * 10)->limit(10)->get();
 
         return view('deadlines.index', [
             'exams' => $exams,
